d8fa6efeedd15ae622695afbb0a08360d8d5c167
Santiago Ramirez <snt.ramirez@gmail.com>
2024-08-23 16:57:27 -0500

    exception

--- src/main/java/com/dev/accountservice/application/api/CuentaApi.java
+++ src/main/java/com/dev/accountservice/application/api/CuentaApi.java
@@ -1,23 +1,16 @@
 package com.dev.accountservice.application.api;
 
-import com.dev.accountservice.domain.core.model.CuentaDto;
 import com.dev.accountservice.domain.core.CuentaServicio;
-import com.dev.accountservice.domain.core.model.RespuestaMovimiento;
-import com.dev.accountservice.domain.core.model.SaldoNoDisponibleException;
-import com.dev.accountservice.domain.core.model.ValidationException;
+import com.dev.accountservice.domain.core.model.CuentaDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
